e707cd0d7eda378234f6d1b958261892f3adcb56
Gaurav <gauravdeshmukh425@gmail.com>
2023-12-12 20:45:34 +0530

    1

--- src/First.java
+++ src/First.java
@@ -1,3 +1,11 @@
 public class First {
-
+    String name;
+    int id;
+    public static void main(String[] args)
+    {
+        First obj=new First();
+        obj.name= "Gaurav";
+        obj.id=101;
+        System.out.println(obj.name+"  "+obj.id);
+    }
 }
