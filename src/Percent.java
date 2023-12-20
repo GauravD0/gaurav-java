1fc8891bd86153dbc08af749cac2df9a498f7770
Gaurav <gauravdeshmukh425@gmail.com>
2023-12-20 16:11:53 +0530

    1

---
+++ src/Percent.java
@@ -0,0 +1,12 @@
+public class Percent {
+    public static void main(String[] args) {
+        float m1=78;
+        float m2=90;
+        float m3=86;
+        float m4=79;
+        float m5=89;
+        float total= m1+m2+m3+m4+m5;
+        float percent=(total*100)/500;
+        System.out.println("percentage= "+percent);
+    }
+}
